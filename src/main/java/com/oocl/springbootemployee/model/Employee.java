7e6590261e59e6b2ea40cde2aec1c45298290f0b
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-26 15:11:13 +0800

    [Spring Boot JPA] feat: make employee entity

--- src/main/java/com/oocl/springbootemployee/model/Employee.java
+++ src/main/java/com/oocl/springbootemployee/model/Employee.java
@@ -1,11 +1,22 @@
 package com.oocl.springbootemployee.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
     private String name;
+
     private Integer age;
+
+    @Enumerated(EnumType.STRING)
     private Gender gender;
+
     private Double salary;
+
     private Boolean isActive = true;
 
     public Employee(Integer id, String name, Integer age, Gender gender, Double salary) {
@@ -28,28 +39,28 @@ public class Employee {
         return name;
     }
 
-    public Integer getAge() {
-        return age;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
     }
 
     public void setAge(Integer age) {
         this.age = age;
     }
 
+    public Gender getGender() {
+        return gender;
+    }
+
     public void setGender(Gender gender) {
         this.gender = gender;
+    }
+
+    public Double getSalary() {
+        return salary;
     }
 
     public void setSalary(Double salary) {
