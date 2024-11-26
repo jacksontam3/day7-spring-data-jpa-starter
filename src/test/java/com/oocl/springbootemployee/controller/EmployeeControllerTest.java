660cf32fd15582b240433f40d74746a1f90d72f7
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-26 17:14:46 +0800

    [Spring Boot JPA] feat: migrate delete employee API to JPA

--- src/test/java/com/oocl/springbootemployee/controller/EmployeeControllerTest.java
+++ src/test/java/com/oocl/springbootemployee/controller/EmployeeControllerTest.java
@@ -206,7 +206,7 @@ class EmployeeControllerTest {
         // Then
         client.perform(MockMvcRequestBuilders.delete("/employees/" + givenId))
             .andExpect(MockMvcResultMatchers.status().isNoContent());
-        List<Employee> employees = employeeInMemoryRepository.findAll();
+        List<Employee> employees = employeeRepository.findAll();
         assertThat(employees).hasSize(4);
         assertThat(employees.get(0).getId()).isEqualTo(2);
         assertThat(employees.get(1).getId()).isEqualTo(3);
