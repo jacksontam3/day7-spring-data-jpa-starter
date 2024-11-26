2671fe95d366f28219f733a8c56f906396b59f66
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-26 16:08:44 +0800

    [Spring Boot JPA] feat: migrate get all employees by gender API to JPA

--- src/main/java/com/oocl/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/oocl/springbootemployee/repository/EmployeeRepository.java
@@ -1,11 +1,17 @@
 package com.oocl.springbootemployee.repository;
 
 import com.oocl.springbootemployee.model.Employee;
+import com.oocl.springbootemployee.model.Gender;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    List<Employee> getAllByGender(Gender gender);
 
 }
