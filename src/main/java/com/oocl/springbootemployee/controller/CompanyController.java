3ba40d94fd24293d00c882fa26368f784f09d53d
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-26 22:02:09 +0800

    [Spring Boot JPA] feat: update pagination logic of company controller

--- src/main/java/com/oocl/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/oocl/springbootemployee/controller/CompanyController.java
@@ -4,6 +4,7 @@ import com.oocl.springbootemployee.model.Company;
 import com.oocl.springbootemployee.model.Employee;
 import com.oocl.springbootemployee.service.CompanyService;
 import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
