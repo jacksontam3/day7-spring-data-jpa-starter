6c87a4ab15609e38ce54ae33d7d72a00911670aa
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-26 21:48:02 +0800

    [Spring Boot JPA] feat: adopt JPA repository in service level of company

--- src/main/java/com/oocl/springbootemployee/service/CompanyService.java
+++ src/main/java/com/oocl/springbootemployee/service/CompanyService.java
@@ -3,6 +3,7 @@ package com.oocl.springbootemployee.service;
 import com.oocl.springbootemployee.model.Company;
 import com.oocl.springbootemployee.model.Employee;
 import com.oocl.springbootemployee.repository.CompanyInMemoryRepository;
+import com.oocl.springbootemployee.repository.CompanyRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,13 +11,15 @@ import java.util.List;
 @Service
 public class CompanyService {
     private final CompanyInMemoryRepository companyInMemoryRepository;
+    private final CompanyRepository companyRepository;
 
-    public CompanyService(CompanyInMemoryRepository companyInMemoryRepository) {
+    public CompanyService(CompanyInMemoryRepository companyInMemoryRepository, CompanyRepository companyRepository) {
         this.companyInMemoryRepository = companyInMemoryRepository;
+        this.companyRepository = companyRepository;
     }
 
     public List<Company> findAll(){
-        return companyInMemoryRepository.findAll();
+        return companyRepository.findAll();
     }
 
     public List<Company> findAll(int pageIndex, int pageSize) {
